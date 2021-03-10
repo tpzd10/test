49d15ec85438ec7586679ec7fbc0089459721a25
tpzd10 <80368238+tpzd10@users.noreply.github.com>
2021-03-09 19:04:50 -0600

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,5 +1,5 @@
 echo "Setting up test tpzd package"
 sudo apt-get install wget
 wget https://raw.githubusercontent.com/tpzd10/test/main/scripts/script2.sh
-sh script2sh
+sh script2.sh
 echo "script 1 complete."
